abcc9ad645bf9a7845a79a80a9216235b2d2a6ed
lzackx <lzackx@lzackx.com>
2018-04-02 15:09:46 +0800

    Add AV project

---
+++ iOS/AV/FFmpeg/FFmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFmpeg
+//
+//  Created by lzackx on 2018/4/2.
+//  Copyright © 2018年 lzackx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
